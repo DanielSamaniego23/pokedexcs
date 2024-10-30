deac02a5ae3393394b13cde5dec69a97360b053a
danielsamaniego370 <danielsamaniego370@gmail.com>
2024-10-30 04:25:17 +0000

    commit

--- src/app/Components/graphs/graphs.component.ts
+++ src/app/Components/graphs/graphs.component.ts
@@ -1,18 +1,26 @@
+// graphs.component.ts
 import { Component } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-graphs',
   standalone: true,
-  imports: [NgIf],
+  imports: [CommonModule],
   templateUrl: './graphs.component.html',
   styleUrl: './graphs.component.css'
 })
 export class GraphsComponent {
-  idPokemon: string = '#001';  // Ejemplo de atributos
-  tipoPokemon: string= 'Agua';
-  nombrePokemon: string = 'XD';
+  idPokemon: string = '#001';
+  tipoPokemon: string = 'Agua';
+  nombrePokemon: string = 'Squirtle';
   vidaPokemon: number = 100;
   ataquePokemon: number = 100;
   defensaPokemon: number = 100;
 
+  // Si quieres formatear el ID con ceros a la izquierda, puedes crear un getter
+  get formattedId(): string {
+    // Elimina el # si existe y añade los ceros
+    const numericId = this.idPokemon.replace('#', '');
+    return '#' + numericId.padStart(3, '0');
+  }
 }
--- src/app/Components/pokedex-view/pokedex-view.component.ts
+++ src/app/Components/pokedex-view/pokedex-view.component.ts
@@ -1,23 +1,47 @@
+// pokedex-view.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GraphsComponent } from "../graphs/graphs.component";
 import { PokedexComponent } from '../pokedex/pokedex.component';
 
 @Component({
   selector: 'app-pokedex-view',
   standalone: true,
-  imports: [GraphsComponent, PokedexComponent],
+  imports: [CommonModule, GraphsComponent, PokedexComponent],
   templateUrl: './pokedex-view.component.html',
   styleUrl: './pokedex-view.component.css'
 })
 export class PokedexViewComponent {
-  pokemonId: number = 0;
-  pokemonImagen:string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.pokemonId+ '.png';
-  changePokemon() {
-    this.pokemonId++;
-    this.pokemonImagen  = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.pokemonId+ '.png';
+  pokemonId: number = 1;
+  pokemonImagen: string = '';
+  currentPokemon: any = {
+    id: 1,
+    nombre: 'Bulbasaur',
+    tipo: 'Grass',
+    vida: 45,
+    ataque: 49,
+    defensa: 49
+  };
+
+  constructor() {
+    this.actualizarPokemon();
   }
+
+  actualizarPokemon() {
+    this.pokemonImagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`;
+  }
+
+  changePokemon() {
+    if (this.pokemonId < 898) { // Límite de Pokémon
+      this.pokemonId++;
+      this.actualizarPokemon();
+    }
+  }
+
   AnteriorPokemon() {
-    this.pokemonId--;
-    this.pokemonImagen  = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.pokemonId+ '.png';
+    if (this.pokemonId > 1) {
+      this.pokemonId--;
+      this.actualizarPokemon();
+    }
   }
 }
